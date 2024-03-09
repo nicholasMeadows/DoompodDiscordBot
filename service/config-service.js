34c2117923a0ba729335503e88297234d072e2e4
nicholasMeadows <meadowsnick98@gmail.com>
2024-03-09 16:57:20 -0600

    Remove unused config service fields

--- service/config-service.js
+++ service/config-service.js
@@ -1,10 +1,6 @@
 module.exports = class ConfigService {
     #token;
     #clientId;
-    #doompodSuckletCron;
-    #itsFridayInCaliforniaCron;
-    #ladiesAndGentlemenTheWeekendCron;
-    #itsWednesdayMyDudesCron;
     #randomActuallyReplyPercentage;
     #databaseFile;
 
@@ -14,10 +10,6 @@ module.exports = class ConfigService {
     loadUpdatedConfig(config) {
         this.#token = config.token;
         this.#clientId = config.clientId;
-        this.#doompodSuckletCron = config.doompodSuckletCron;
-        this.#itsFridayInCaliforniaCron = config.itsFridayInCaliforniaCron;
-        this.#ladiesAndGentlemenTheWeekendCron = config.ladiesAndGentlemenTheWeekendCron;
-        this.#itsWednesdayMyDudesCron = config.itsWednesdayMyDudesCron;
         this.#randomActuallyReplyPercentage = config.randomActuallyReplyPercentage;
         this.#databaseFile = config.databaseFile;
     }
@@ -27,18 +19,6 @@ module.exports = class ConfigService {
     }
     getClientId() {
         return this.#clientId;
-    }
-    getDoompodSuckletCron() {
-        return this.#doompodSuckletCron;
-    }
-    getItsFridayInCaliforniaCron() {
-        return this.#itsFridayInCaliforniaCron;
-    }
-    getLadiesAndGentlemenTheWeekendCron() {
-        return this.#ladiesAndGentlemenTheWeekendCron;
-    }
-    getItsWednesdayMyDudesCron() {
-        return this.#itsWednesdayMyDudesCron;
     }
 
     getRandomActuallyReplyPercentage() {
