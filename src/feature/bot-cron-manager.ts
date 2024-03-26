fc72da71b3a9444a88d66f7624dc51a9633741e6
nicholasMeadows <meadowsnick98@gmail.com>
2024-03-26 10:55:18 -0500

    Add daily capybara cron action

--- src/feature/bot-cron-manager.ts
+++ src/feature/bot-cron-manager.ts
@@ -3,9 +3,11 @@ import DiscordClient from "../model/discord-client";
 import {Repositories} from "../model/mongo-db-info";
 import GuildChannelCronInfo from "../model/guild-channel-cron-info";
 import CronAction from "../model/enum/cron-action";
-import {TextChannel} from "discord.js";
+import {AttachmentBuilder, TextChannel} from "discord.js";
 import WalkCompetitionFeature from "./walk-competition-feature";
 import Log from "../log";
+import {DAILY_CAPYBARA_URL} from "../constants";
+import DailyCapybaraResponse from "../model/daily-capybara-response";
 
 export default class BotCronManager {
     private _discordClient: DiscordClient;
@@ -52,6 +54,9 @@ export default class BotCronManager {
             case CronAction.POST_WALKING_RESULTS:
                 this.handlePostWalkingResultsCron(guildChannelCronInfo);
                 break;
+            case CronAction.DAILY_CAPYBARA:
+                this.handleDailyCapybara(guildChannelCronInfo);
+                break;
         }
     }
 
@@ -88,5 +93,39 @@ export default class BotCronManager {
     private async handlePostWalkingResultsCron(guildChannelCronInfo: GuildChannelCronInfo) {
         const walkCompetitionFeature = new WalkCompetitionFeature(this._discordClient, this._repositories);
         walkCompetitionFeature.handlePostWalkingResultsCron(guildChannelCronInfo);
+    }
+
+    async handleDailyCapybara(guildChannelCronInfo: GuildChannelCronInfo) {
+        const channelDiscordId = guildChannelCronInfo.channelDiscordId;
+        const discordChannel = await this._discordClient.channels.fetch(channelDiscordId);
+        if (discordChannel === null) {
+            this.logger.warn(`discord client returned null when fetching channel with discord id ${channelDiscordId}`);
+            return;
+        }
+
+        const dailyCapybaraResponse = await fetch(DAILY_CAPYBARA_URL, {
+            method: 'GET',
+            headers: {
+                Accept: 'application.json',
+                'Content-Type': 'application/json'
+            }
+        });
+        const responseText = await dailyCapybaraResponse.text();
+        const responseJson: DailyCapybaraResponse = JSON.parse(responseText);
+
+        const imageResponse = await fetch(responseJson.image);
+
+        const imageArrayBuffer = await imageResponse.arrayBuffer();
+        const buffer = Buffer.from(imageArrayBuffer);
+
+        const file = new AttachmentBuilder(buffer);
+        await (discordChannel as TextChannel).send({
+            content: `Name: ${responseJson.name}\n` +
+                `Class: ${responseJson.class}\n` +
+                `Muncher lvl: ${responseJson.muncher_lvl}\n` +
+                `Relationship status: ${responseJson.relationship_status}\n` +
+                `Weapon of choice: ${responseJson.weapon}\n`,
+            files: [file]
+        })
     }
 }
---
+++ src/model/daily-capybara-response.ts
@@ -0,0 +1,11 @@
+export default interface DailyCapybaraResponse {
+    name: string;
+    image: string;
+    _id: string;
+    muncher_lvl: number;
+    weapon: string;
+    class: string;
+    used: string;
+    relationship_status: string;
+    days_ago: number;
+}
